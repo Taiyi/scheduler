07c48a89f9a7533a11da9c82fdc0c4d86da55e9b
Taiyi <Taiyi@users.noreply.github.com>
2018-02-21 13:58:39 -0800

    commit 2

--- main.rb
+++ main.rb
@@ -1,19 +1,37 @@
 require 'sinatra' 
 require 'haml'
 require 'yaml'
+require 'date'
 
-$engineerList = YAML.load_file('Engineers.yml')['engineers']
 
+class Schedule
+	@schedule = YAML.load_file('schedule.yml')
+end
 
 def getEngineer()
 	#Returns a random engineer from engineers.yml.
-	$engineerList.sample
+	engineerList = YAML.load_file('engineers.yml')['engineers']
+	engineerList.sample
+end
+
+def getSchedule()
+	#Returns last saved schedule
+	YAML.load_file('schedule.yml')
+end
+
+def saveSchedule()
+	#Saves current schedule
+	File.open('schedule.yml', 'w') {|f| f.write d.to_yaml }
+end
+
+def removeEntry(schedule)
+	#removes entry from current schedule
 end
 
 get '/' do 
-  haml :index 
+	haml :index 
 end
 
 get '/sdf' do 
-  "test" 
+	"test" 
 end
