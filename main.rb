e4c2b50af38d9625774c9ffd852f5eae65e26f96
Taiyi <Taiyi@users.noreply.github.com>
2018-02-23 14:49:38 -0800

    commit 8

--- main.rb
+++ main.rb
@@ -3,6 +3,8 @@ require 'haml'
 require 'yaml'
 require 'date'
 require 'json'
+require 'gon-sinatra'
+Sinatra::register Gon::Sinatra
 
 #rerun 'ruby main.rb'
 
@@ -30,30 +32,38 @@ end
 
 get '/' do 
 	@engineerList = YAML.load_file('engineers.yml')['engineers']
+	$schedule = YAML.load_file('schedule.yml')
+	gon.schedule = $schedule
 	haml :index 
 end
 
-get '/:target/:selected' do
+get '/:target/:selected/:year/:month/:day' do
 	@target = params[:target]
 	@selected = params[:selected]
+	@year = params[:year].to_i
+	@month = params[:month].to_i+1 #month is returned 0-indexed so increment by 1 for readability
+	@day = params[:day].to_i #monday of week and considered the first day
 
-	@temp = $schedule[@target]
+	@dateArray = [@year, @month, @day]
 
-	if $schedule.key?(@selected)
-		$schedule[@target] = $schedule[@selected]
+	if @target == "None"
+		$schedule[@selected] = @dateArray
 	else
-		$schedule.delete(@selected)
+		@temp = $schedule[@target]
+		if $schedule.key?(@selected) 
+			$schedule[@target] = $schedule[@selected]
+		else
+			$schedule.delete(@selected)
+		end
+		$schedule[@selected] = @temp
 	end
-
-	$schedule[@selected] = @temp
-
-	puts $schedule
 
 	status 204
 end
 
 get '/save' do
-	"hello world"
+	File.open('schedule.yml', 'w') {|f| f.write $schedule.to_yaml }
+	status 204
 end
 
 get '/revert' do
--- public/scripts/myscript.js
+++ public/scripts/myscript.js
@@ -27,32 +27,93 @@ function fillCalander(dateString) {
 	var start = d.getDay();	//
 	var end = 32 - new Date(yearInt, monthInt, 32).getDate();	//
 
-	var lastMonth = new Date()
+	var lastMonth = d;
 	lastMonth.setDate(1);
 	lastMonth.setHours(-1);
 	lastMonth = lastMonth.getDate()-start+1; //Gets the first visible date on calender
 
 
+
 	var count = 1;
+	var otherMonth = false;
 	for (x=0; x<5; x++)
 	{
+		//Reset all assigned engineer entries to None
+		var e = document.getElementById('e' + x);
+		e.innerHTML = "None";
+
 		for (y=0; y<7; y++)
 		{
+			var cellID = x+"x"+y;
+			var d = document.getElementById(cellID);
 			if ((x==0 && y>=start) || (x!=0))
+				//if date is in active month
 			{
-				var cellID = x+"x"+y;
-				document.getElementById(cellID).innerHTML = count;
+				d.classList.remove("previousMonth");
+				d.classList.remove("nextMonth");
+				d.innerHTML = count;
 				count++;
+
+				if (otherMonth == true)
+				{
+					//if date is in next month
+					d.className += " nextMonth";
+				}
 				if (count > end)
 				{
 					count = 1;
+					otherMonth = true;
 				}
 			}
 			else if (x==0 && y<start)
 			{
-				var cellID = x+"x"+y;
-				document.getElementById(cellID).innerHTML = lastMonth;
+				//if date is in last month
+				d.innerHTML = lastMonth;
 				lastMonth++;
+				d.className += " previousMonth"
+			}
+
+			if (y==1)
+			{
+				//check if any engineer is active this week
+				Object.keys(gon.schedule).forEach(function (key) {
+					var dateArray = gon.schedule[key]// [year, month, day]
+
+					var currentYear = activeMonthYear.getFullYear();
+					var currentMonth = activeMonthYear.getMonth();
+					var currentDay = d.innerHTML;
+
+					e = document.getElementById('e' + x);
+					if (dateArray[2] == currentDay)
+					{
+						if (dateArray[0] == currentYear && dateArray[1]-1 == currentMonth)
+							//Month was stored 1-incremented so reduce by 1
+						{
+							//Current Month
+							e.innerHTML = key;
+							console.log(key, dateArray)
+							console.log(currentYear, currentMonth+1, currentDay)
+						}
+						else if (dateArray[1]-1 == currentMonth-1)
+						{
+							//Past Month
+							if (dateArray[0] == currentYear || (currentMonth == 0 && dateArray[0] == currentYear-1))
+								//Checks year.
+							{
+								e.innerHTML = key;
+							}
+						}
+						else if (dateArray[1]-1 == currentMonth+1)
+						{
+							//Next Month
+							if (dateArray[0] == currentYear || (currentMonth == 0 && dateArray[0] == currentYear+1))
+							{
+								e.innerHTML = key;
+							}
+						}
+
+					}
+				});
 			}
 		}
 	}
@@ -72,7 +133,8 @@ document.getElementById('nextMonth').onclick = function(){
 };
 
 document.getElementById('dropdown').onchange = function(){
-var selected = document.getElementById('dropdown').value;
+
+	var selected = document.getElementById('dropdown').value;
 	if (selected == "---")
 	{
 		return;
@@ -87,10 +149,37 @@ var selected = document.getElementById('dropdown').value;
 		var entry = entries[x];
 		var week = entry.id[1];
 		var dayID = week+'x'+1;
-		var day = document.getElementById(dayID).innerHTML;
+		var dayElement = document.getElementById(dayID);
+		var day = dayElement.innerHTML
 		var month = activeMonthYear.getMonth();
 		var year = activeMonthYear.getFullYear();
 
+		if (dayElement.classList.contains("previousMonth"))
+		{
+			//this monday belongs to the previous month
+			if (month == 0)
+			{
+				month = 11;
+				year--;
+			}
+			else
+			{
+				month--;
+			}
+		}
+		else if (dayElement.classList.contains("nextMonth"))
+		{
+			//this monday belongs to the next month
+			if (month == 11)
+			{
+				month = 0;
+				year++;
+			}
+			else
+			{
+				month++;
+			}
+		}
 
 		entry.href = "/" + entry.innerHTML + "/" + selected  + "/" + year + "/" + month + "/" + day;
 	}
