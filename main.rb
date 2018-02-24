8412873e90f23f0f4fc0c8db7938b91884b88037
Taiyi <Taiyi@users.noreply.github.com>
2018-02-24 02:03:48 -0800

    added unit tests

--- main.rb
+++ main.rb
@@ -4,46 +4,76 @@ require 'yaml'
 require 'gon-sinatra'
 Sinatra::register Gon::Sinatra
 
-$schedule = YAML.load_file('schedule.yml')
+def resetSchedule()
+   $schedule = {}
+   saveSchedule($schedule)
+end
+
+def getSchedule()
+	return YAML.load_file('schedule.yml')
+end
+
+def saveSchedule(schedule)
+	File.open('schedule.yml', 'w') {|f| f.write schedule.to_yaml }
+	return
+end
+
+def addEngineer(schedule, selected, year, month, day)
+	dateArray = [year, month, day]
+	schedule[selected] = dateArray
+end
+
+def deleteEngineer(schedule, target)
+	schedule.delete(target)
+end
+
+def switchEngineers(schedule, target, selected)
+	temp = schedule[target]
+	if schedule.key?(selected) 
+		schedule[target] = schedule[selected]
+	else
+		schedule.delete(target)
+	end
+	schedule[selected] = temp
+end
+
+$schedule = getSchedule()
 
 get '/' do 
 	@engineerList = YAML.load_file('engineers.yml')['engineers']
-	$schedule = YAML.load_file('schedule.yml')
+	$schedule = getSchedule()
 	gon.schedule = $schedule
 	haml :index 
 end
 
 get '/:target/:selected/:year/:month/:day' do
-	@target = params[:target]
-	@selected = params[:selected]
-	@year = params[:year].to_i
-	@month = params[:month].to_i+1 #month is returned 0-indexed so increment by 1 for readability
-	@day = params[:day].to_i #monday of week and considered the first day
+	target = params[:target]
+	selected = params[:selected]
+	year = params[:year].to_i
+	month = params[:month].to_i+1 #month is returned 0-indexed so increment by 1 for readability
+	day = params[:day].to_i #monday of week and considered the first day
 
-	@dateArray = [@year, @month, @day]
-
-	if @target == "None"
-		$schedule[@selected] = @dateArray
-	elsif @selected == "None"
-		$schedule.delete(@target)
-	else
-		@temp = $schedule[@target]
-		if $schedule.key?(@selected) 
-			$schedule[@target] = $schedule[@selected]
-		else
-			$schedule.delete(@target)
-		end
-		$schedule[@selected] = @temp
+	if target == "None"
+		addEngineer($schedule, selected, year, month, day)
+	elsif selected == "None"
+		deleteEngineer($schedule, target)
+	elsif  target != "None" and selected != "None"
+		switchEngineers($schedule, target, selected, year, month, day)
 	end
 
 	status 204
 end
 
 get '/save' do
-	File.open('schedule.yml', 'w') {|f| f.write $schedule.to_yaml }
+	saveSchedule($schedule)
 	status 204
 end
 
 get '/revert' do 
+	redirect "/", 303
+end
+
+get '/reset' do 
+	resetSchedule()
 	redirect "/", 303
 end
---
+++ spec/main_spec.rb
@@ -0,0 +1,76 @@
+require 'spec_helper'
+require File.join(File.expand_path(".."), 'Scheduler/main.rb')
+
+#run with command 'rspec spec' while in root folder
+#root folder must be named Scheduler
+
+describe "Schedule" do 
+   context "when testing scheduler, function" do 
+      
+      it "should return an empty hash after resetting schedule" do 
+         resetSchedule()
+         $testSchedule = getSchedule()
+         correct = {}
+         $testSchedule.each do |k, v|
+            expect(v).to eq(correct[k])
+         end
+      end
+
+      it "should include new entry after adding to schedule" do   
+         addEngineer($testSchedule, "Brown", 2018, 2, 5);
+         correct = {"Brown"=>[2018, 2, 5]}
+         $testSchedule.each do |k, v|
+            expect(v).to eq(correct[k])
+         end
+         addEngineer($testSchedule, "Ivy", 2018, 1, 29);
+         correct = {"Brown"=>[2018, 2, 5], "Ivy"=>[2018, 1, 29]}
+         $testSchedule.each do |k, v|
+            expect(v).to eq(correct[k])
+         end
+         addEngineer($testSchedule, "Smith", 2017, 10, 26);
+         correct = {"Brown"=>[2018, 2, 5], "Ivy"=>[2018, 1, 29], "Smith"=>[2017, 10, 26]}
+         $testSchedule.each do |k, v|
+            expect(v).to eq(correct[k])
+         end
+      end
+
+      it "should switch the weeks of two preexisting engineers" do
+         switchEngineers($testSchedule, "Brown", "Smith");
+         correct = {"Smith"=>[2018, 2, 5], "Ivy"=>[2018, 1, 29], "Brown"=>[2017, 10, 26]}
+         $testSchedule.each do |k, v|
+            expect(v).to eq(correct[k])
+         end
+         switchEngineers($testSchedule, "Ivy", "Smith");
+         correct = {"Ivy"=>[2018, 2, 5], "Smith"=>[2018, 1, 29], "Brown"=>[2017, 10, 26]}
+         $testSchedule.each do |k, v|
+            expect(v).to eq(correct[k])
+         end
+      end
+
+      it "remove engineers that are deleted" do
+         deleteEngineer($testSchedule, 'Ivy')
+         correct = {"Smith"=>[2018, 1, 29], "Brown"=>[2017, 10, 26]}
+         $testSchedule.each do |k, v|
+            expect(v).to eq(correct[k])
+         end
+      end
+
+      it "should do nothing when a nonexistent engineer is deleted" do
+         deleteEngineer($testSchedule, 'Ivy')
+         correct = {"Smith"=>[2018, 1, 29], "Brown"=>[2017, 10, 26]}
+         $testSchedule.each do |k, v|
+            expect(v).to eq(correct[k])
+         end
+      end
+
+      it "should commit schedule to yml when saving" do
+         correct = $testSchedule
+         saveSchedule($testSchedule)
+         $testSchedule = getSchedule()
+         $testSchedule.each do |k, v|
+            expect(v).to eq(correct[k])
+         end
+      end
+
+   end
+end
